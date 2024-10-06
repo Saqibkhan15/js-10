e34283885c3d021fe93ed81dffac95d6b3bee5ac
Saqibkhan15 <aqib12u@gmail.com>
2024-10-06 11:37:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,33 @@
 // alert(index);
 
 //5 
-var word = "Pakistani";
-var charAt3 = word.charAt(3);
-alert(charAt3)
+// var word = "Pakistani";
+// var charAt3 = word.charAt(3);
+// alert(charAt3)
+
+//6
+// var firstName = prompt("write first name")
+// var lastName = prompt("write last name")
+// var fullName= alert(firstName+lastName)
+//7
+// var text = "hyderabad"
+// var result = text.replace("hyder", "islam")
+// alert(result)
+//8
+//var text = ("Ali and Sami are best friends. They play cricket and football together")
+//9
+// var text1 = "473"
+// console.log(typeof text1)
+// var number = 473
+// console.log(typeof number)
+
+//10
+
+// var texttoUppercase = "peanut"
+// console.log(texttoUppercase.toUpperCase())
+
+//11 
+// var titleCase = "javascript"
+// console.log(titleCase.charAt(0).toUpperCase())
 
 
-var text = ("hydrabad")
-var result = text.replace(/hydra/, "islam")
