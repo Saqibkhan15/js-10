128b1c81c70f7b036bfdb870947eb8b290ff7968
Saqibkhan15 <aqib12u@gmail.com>
2024-10-06 08:54:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// //1-
+// var firstName = prompt("write first name")
+// var lastName = prompt("write last name")
+// var fullName= alert(firstName+lastName)
+// //2-
+// var phoneName = prompt("write favorite phone")
+// alert(phoneName)
+// alert(phoneName.length)
+//3-
+// var city = ["pakistan"];
+// var index = city[0].indexOf("n");
+// alert(index);
+//4
+// var greet = ["hello world"];
+// var index = greet[0].lastIndexOf("l");
+// alert(index);
+
+//5 
+var word = "Pakistani";
+var charAt3 = word.charAt(3);
+alert(charAt3)
+
+
+var text = ("hydrabad")
+var result = text.replace(/hydra/, "islam")
